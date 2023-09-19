c1f1028bdee7d22983a7389009f718399d7143d9
apridew <dewiapriyantiputra@gmail.com>
2023-09-19 04:06:34 -0400

    add login and signup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from './Navbar/Navbar';
-import Landing_Page from './Landing_Page/Landing_Page';
+import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/signup" element={<Sign_Up/>}/>
               </Routes>
         </BrowserRouter>
        
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Navbar.css';
 
 function Navbar() {
@@ -35,22 +36,22 @@ function Navbar() {
       </div>
       <ul className="nav__links active">
         <li className="link">
-          <a href="../Landing_Page/LandingPage.html">Home</a>
+            <Link to="/">Home</Link>
         </li>
         <li className="link">
-          <a href="#">Appointments</a>
+            <Link to="/appointments">Appointments</Link>
         </li>
         <li className="link">
-          <a href="../Sign_Up//Sign_Up.html">
+            <Link to="/signup">
             <button className="btn1">Sign Up</button>
-          </a>
+            </Link>
         </li>
         <li className="link">
-          <a href="../Login/Login.html">
+            <Link to="/login">
             <button className="btn1">Login</button>
-          </a>
+            </Link>
         </li>
-      </ul>
+        </ul>
     </nav>
   );
 }
